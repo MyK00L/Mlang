561874460616307bee2e53ecea8115bbe6a412b5
MyK_00L <michaelchelli00@gmail.com>
2018-08-02 22:30:14 +0200

    Changed memory reserving algorithm

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -8,34 +8,43 @@
 
 using namespace std;
 
+const int BLOCK_SIZE = 4096;
+const int N_BLOCKS = 4096;
+
 const uint8_t b64[256] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,62,0,0,0,63,52,53,54,55,56,57,58,59,60,61,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,0,0,0,0,0,0,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 inline int get64(const string& s, int& i){
-    int res = b64[s[i]];
-    while(s[++i]!=';'){ //terminating char for b64 number
-        res=(res<<6)+b64[s[i]];
+    int res = b64[(int)s[i]];
+    while(s[++i]!=(int)';'){ //terminating char for b64 number
+        res=(res<<6)+b64[(int)s[i]];
     }
     ++i; //align with next readable thing
     return res;
 }
 
-int * vars;
-int   varn;
+int * vars[N_BLOCKS];
+int   blockn;
 inline int& var(int id){
-    if(varn<=id){
-        varn = id<<1;
-        vars = (int *)realloc(vars,varn*sizeof(int));
+    while(blockn*BLOCK_SIZE<id){
+        vars[blockn++] = (int *)malloc(BLOCK_SIZE*sizeof(int));
+	if(!vars[blockn]){
+            cerr<<"Could not allocate " << blockn*BLOCK_SIZE*sizeof(int) << " bytes of memory"<<endl;
+            --blockn;exit();
+            throw;
+        }
     }
-    return vars[id];
+    return vars[id/BLOCK_SIZE][id%BLOCK_SIZE];
 }
 
 //initialize
 inline void init(){
-    varn = 64;
-    vars = (int *)malloc(varn*sizeof(int));
+    blockn = 1;
+    vars[0] = (int *)malloc(BLOCK_SIZE*sizeof(int));
 }
 
 inline void exit(){
-    free(vars);
+    for(int i=0; i<blockn; ++i){
+	free(vars[i]);
+    }
 }
 
 inline void readProgram(vector<string>& v, const string& filename){
@@ -158,16 +167,18 @@ inline void executeProgram(vector<string>& program){
             i=line-1;
         } else if(program[i][0]=='p'){
             j=2;//for [
-            cout<<eval(program[i],j)<<endl;
+            cout<<eval(program[i],j)<<'\n';
         }
     }
 }
 
 int main(int argc, char ** argv){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     init();
     vector<string> p;
     readProgram(p,argv[1]);
-    for(int i=0; i<p.size(); ++i){
+    for(unsigned int i=0; i<p.size(); ++i){
         cerr<<i<<":\t"<<p[i]<<endl;
     }
     executeProgram(p);
